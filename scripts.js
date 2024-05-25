5622ce43689cc9eec3ca4d4c9c024101f665160a
kamranana <155221587+kamranana@users.noreply.github.com>
2024-05-25 17:11:28 -0400

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.querySelector('.submit-button').addEventListener('click', function() {
+    const comment = document.querySelector('#comment').value;
+    alert('Your comment: ' + comment);
+});
